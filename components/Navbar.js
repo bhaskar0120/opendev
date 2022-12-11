dfa75f684f4d99324abfdb225530a87a6cd86ffd
bhaskar0120 <bhaskar0120@gmail.com>
2022-12-12 04:57:31 +0530

    Made Navbar responsive

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,16 @@
-import { Text, Navbar, Button } from '@nextui-org/react';
+import { Link, Text, Navbar, Button } from '@nextui-org/react';
 
 export default function App() {
+    let collapseItems = ["Home", "Courses", "About me"]
     return (
         <Navbar variant={"floating"} isBordered >
             <Navbar.Brand>
-                <Text h2 weight={"bold"}> {"<Open"} </Text>
-                <Text h2 weight={"bold"} color={'primary'}> Dev </Text>
-                <Text h2 weight={"bold"}> {"/>"} </Text>
+                <Navbar.Toggle showIn={'md'} aria-label='toggle navigation' />
+                <Text hideIn={'md'} h2 weight={"bold"}> {"<Open"} </Text>
+                <Text hideIn={'md'} h2 weight={"bold"} color={'primary'}> Dev </Text>
+                <Text hideIn={'md'} h2 weight={"bold"}> {"/>"} </Text>
             </Navbar.Brand>
-            <Navbar.Content activeColor={"primary"} variant={"highlight-solid-rounded"}>
+            <Navbar.Content hideIn={"md"} activeColor={"primary"} variant={"highlight-solid-rounded"}>
                 <Navbar.Link href="#">Home</Navbar.Link>
                 <Navbar.Link href="#">Courses</Navbar.Link>
                 <Navbar.Link isActive={true} href="#">About me</Navbar.Link>
@@ -17,6 +19,21 @@ export default function App() {
                 <Navbar.Link isActive href="#"><Text size="$xl">Login</Text></Navbar.Link>
                 <Button auto color={"neutral"}>Sign Up</Button>
             </Navbar.Content>
+            <Navbar.Collapse>
+                {collapseItems.map((item, index) => (
+                    <Navbar.CollapseItem key={item}>
+                        <Link
+                            color="inherit"
+                            css={{
+                                minWidth: "100%",
+                            }}
+                            href="#"
+                        >
+                            {item}
+                        </Link>
+                    </Navbar.CollapseItem>
+                ))}
+            </Navbar.Collapse>
         </Navbar>
     );
 }
