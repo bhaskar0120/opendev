c747f5e8415ab6c4f6d10a594762b29d260eedcb
bhaskar0120 <bhaskar0120@gmail.com>
2022-12-14 08:16:42 +0530

    Moved Example to API

--- pages/posts/[PID].js
+++ pages/posts/[PID].js
@@ -11,7 +11,6 @@ export default function App(props) {
     const PID = router.query.PID
 
     return (
-
         <Layout>
             <div className={Style.markdown} dangerouslySetInnerHTML={{ __html: (props.data) }} />
             <Script onLoad={() => PR.prettyPrint()} src="https://cdnjs.cloudflare.com/ajax/libs/prettify/r298/prettify.min.js" />
@@ -20,95 +19,8 @@ export default function App(props) {
 }
 
 export async function getServerSideProps() {
-    // Return multiline dummy markdown
-    let markdown = `
-# This is a heading
-## This is a sub-heading
-### This is a sub-sub-heading
-[This is a link](https://www.google.com)
-
-Try an image
-
-![This is an image](https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png)
-
-Try an image to be a link
-[![This is an image](https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png)](https://www.google.com)
-
-This is a paragraph
-
-Some inline code \`const a = 1;\`  and some more inline code \`const b = 2;\`
-
-Some bold text **This is bold text**
-
-Some italic text *This is italic text*
-
-Some strikethrough text ~~This is strikethrough text~~
-
-Some bold and italic text ***This is bold and italic text***
-
-Some Bold and strikethrough text **~~This is bold and strikethrough text~~**
-
-Some italic and strikethrough text *~~This is italic and strikethrough text~~*
-
-Some bold, italic and strikethrough text ***~~This is bold, italic and strikethrough text~~***
-
-
-This is another paragraph
-List inside a list inside a list
-1. Item 1
-    - Item 1.1
-        1. Item 1.1.1
-2. Item 2
-3. Item 3
-
-Checkboxes
-- [x] Item 1
-- [ ] Item 2
-- [ ] Item 3
-
-This is a list
-- Item 1
-- Item 2
-- Item 3
-
-This is a numbered list
-1. Item 1
-3. Item 2
-2. Item 3
-
-This is a code block
-\`\`\`
-void main(){
-    printf("Hello World");
-}
-\`\`\`
-
-Attempt to inject html XSS
-<script>alert("XSS")</script>
-
-This is a table
-| Name | Age |
-| :-: | :-: |
-| John | 20  |
-| Jane | 21  |
-| Jack | 22  |
-
-Attempt to create an XSS button
-<button onclick="alert('XSS')">Click Me</button>
-This is a Blockquote
-> This is a blockquote
-
-List inside a blockquote
-> - Item 1
-> - Item 2
-> - Item 3
-
-<
-`
-
-    // Perform regex substitution to remove html tags <
     const render = {
-        code(string, infostring) {
+        code(string) {
             return `
             <div class="code-block">
                 <code class="prettyprint">${string}</code>
@@ -134,6 +46,10 @@ List inside a blockquote
             </div>`
         },
     }
+
+    let raw = await fetch('http://localhost:3000/api/example')
+    let data = await raw.json()
+    let markdown = data.blog
 
     marked.use({ renderer: render });
     markdown = markdown.replace(/&/g, "&amp;");
