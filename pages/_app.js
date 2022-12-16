0a025c0f579c4b38a891e41abb551bee03626bb4
bhaskar0120 <bhaskar0120@gmail.com>
2022-12-16 07:33:08 +0530

    Added Dark mode config

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,28 @@
 import '../styles/globals.css';
-import { NextUIProvider } from '@nextui-org/react';
+import { NextUIProvider, createTheme } from '@nextui-org/react';
+import { ThemeProvider as NextThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }) {
+    // 2. Call `createTheme` and pass your custom values
+    const lightTheme = createTheme({
+        type: 'light',
+        theme: {}
+    })
+    const darkTheme = createTheme({
+        type: 'dark',
+        theme: {}
+    })
     return (
         // 2. Use at the root of your app
-        <NextUIProvider>
+        <NextThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            value={{ light: lightTheme.className, dark: darkTheme.className }}
+        >
+            <NextUIProvider >
             <Component {...pageProps} />
         </NextUIProvider>
+        </NextThemeProvider>
     );
 }
 
